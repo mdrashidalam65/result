aad44c9ecc665c56acbb3d877868ccb6e0c1ba7d
Md Rashid Alam <71508492+mdrashidalam65@users.noreply.github.com>
2024-04-20 14:40:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Example data (replace with your own)
 const students = [
-    { registrationNo: "21110111057", name: "Md Rashid Alam", rank: 1 },
+    { registrationNo: "21110111057", name: "Md Rashid Alam", rank: 6 },
     { registrationNo: "21110111018", name: "Asmit Kumar", rank: 2 },
-    { registrationNo: "21110111029", name: "Pankaj Kumar", rank: 3 },
-    { registrationNo: "21110111030", name: "Saurabh Kumar", rank: 4 },
-    { registrationNo: "21110111006", name: "Amit Kumar", rank: 5 },
-    { registrationNo: "211101110468", name: "Aman", rank: 6 },
+    { registrationNo: "21110111029", name: "Pankaj Kumar", rank: 1 },
+    { registrationNo: "21110111030", name: "Saurabh Kumar", rank: 3 },
+    { registrationNo: "21110111006", name: "Amit Ranjan", rank: 6 },
+    { registrationNo: "211101110468", name: "Aman", rank: 5 },
     // Add more students
 ];
 
